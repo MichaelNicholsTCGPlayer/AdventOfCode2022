4dd50509e77710d07ada3804da23371ebe3653bc
Michael Nichols <michael.nichols@tcgplayer.com>
2022-12-01 10:05:06 -0500

    Solution For Day 1

--- Puzzles/Solutions/Day01.cs
+++ Puzzles/Solutions/Day01.cs
@@ -6,12 +6,41 @@
 
         public string Puzzle1(string[] input)
         {
-            throw new NotImplementedException();
+            var calories = CreateCaloriesCount(input);
+
+            return calories.Max().ToString();
         }
 
         public string Puzzle2(string[] input)
         {
-            throw new NotImplementedException();
+            var calories = CreateCaloriesCount(input);
+            return calories.OrderByDescending(r => r).Take(3).Sum().ToString();
+        }
+
+        private List<long> CreateCaloriesCount(string[] input)
+        {
+            List<long> calories = new List<long>();
+
+            long currentCalorieCount = 0;
+            foreach (var row in input)
+            {
+                if (row == "")
+                {
+                    calories.Add(currentCalorieCount);
+                    currentCalorieCount = 0;
+                }
+                else
+                {
+                    currentCalorieCount += long.Parse(row);
+                }
+            }
+
+            if (currentCalorieCount != 0)
+            {
+                calories.Add(currentCalorieCount);
+            }
+
+            return calories;
         }
     }
 }
