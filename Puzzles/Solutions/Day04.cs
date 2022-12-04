8b4494f20998e644d04890024f75cd72e8aacd2e
Michael Nichols <michael.nichols@tcgplayer.com>
2022-12-04 07:36:32 -0500

    Day 4 Solution

--- Puzzles/Solutions/Day04.cs
+++ Puzzles/Solutions/Day04.cs
@@ -6,12 +6,82 @@
 
         public string Puzzle1(string[] input)
         {
-            throw new NotImplementedException();
+            var completePairContainsCount = 0;
+            foreach (var item in input)
+            {
+                var split = item.Split(",");
+
+                var resplit1 = split[0].Split("-");
+                var resplit2 = split[1].Split("-");
+
+                var start1 = Int64.Parse(resplit1[0]);
+                var stop1 = Int64.Parse(resplit1[1]);
+                var start2 = Int64.Parse(resplit2[0]);
+                var stop2 = Int64.Parse(resplit2[1]);
+
+                if (start1 < start2)
+                {
+                    if (stop1 >= stop2)
+                    {
+                        completePairContainsCount++;
+                    }
+                }
+                else if (start1 > start2)
+                {
+                    if (stop2 >= stop1)
+                    {
+                        completePairContainsCount++;
+                    }
+                }
+                else
+                {
+                    // Start is equal, one will contain the other
+                    completePairContainsCount++;
+                }
+            }
+
+
+            return completePairContainsCount.ToString();
         }
 
         public string Puzzle2(string[] input)
         {
-            throw new NotImplementedException();
+            var completePairIntersectCount = 0;
+            foreach (var item in input)
+            {
+                var split = item.Split(",");
+
+                var resplit1 = split[0].Split("-");
+                var resplit2 = split[1].Split("-");
+
+                var start1 = Int64.Parse(resplit1[0]);
+                var stop1 = Int64.Parse(resplit1[1]);
+                var start2 = Int64.Parse(resplit2[0]);
+                var stop2 = Int64.Parse(resplit2[1]);
+
+                if (start1 < start2)
+                {
+                    if (stop1 >= start2)
+                    {
+                        completePairIntersectCount++;
+                    }
+                }
+                else if (start1 > start2)
+                {
+                    if (stop2 >= start1)
+                    {
+                        completePairIntersectCount++;
+                    }
+                }
+                else
+                {
+                    // Start is equal, one will contain the other
+                    completePairIntersectCount++;
+                }
+            }
+
+
+            return completePairIntersectCount.ToString();
         }
     }
 }
