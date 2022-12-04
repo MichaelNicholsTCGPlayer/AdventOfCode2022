b0a17d44a47798f7c1045f30a893e1194049a075
Michael Nichols <michael.nichols@tcgplayer.com>
2022-12-04 07:59:23 -0500

    Day 4 minimal code solution

--- Puzzles/Solutions/Day04.cs
+++ Puzzles/Solutions/Day04.cs
@@ -6,82 +6,115 @@
 
         public string Puzzle1(string[] input)
         {
-            var completePairContainsCount = 0;
-            foreach (var item in input)
-            {
-                var split = item.Split(",");
-
-                var resplit1 = split[0].Split("-");
-                var resplit2 = split[1].Split("-");
-
-                var start1 = Int64.Parse(resplit1[0]);
-                var stop1 = Int64.Parse(resplit1[1]);
-                var start2 = Int64.Parse(resplit2[0]);
-                var stop2 = Int64.Parse(resplit2[1]);
-
-                if (start1 < start2)
-                {
-                    if (stop1 >= stop2)
-                    {
-                        completePairContainsCount++;
-                    }
-                }
-                else if (start1 > start2)
-                {
-                    if (stop2 >= stop1)
-                    {
-                        completePairContainsCount++;
-                    }
-                }
-                else
-                {
-                    // Start is equal, one will contain the other
-                    completePairContainsCount++;
-                }
-            }
-
-
-            return completePairContainsCount.ToString();
+            // Minial Code Approach
+            return input.Select(r => r.Split(",")
+                    .Select(q => q.Split("-")
+                        .Select(p => int.Parse(p))
+                        .ToArray())
+                    .ToArray())
+                .Where(r => r[0][0] == r[1][0] || // Start1 == Start2
+                       (r[0][0] < r[1][0] && r[0][1] >= r[1][1]) || // Start1 < Start2 + Stop1 >= Stop2
+                       (r[0][0] > r[1][0] && r[0][1] <= r[1][1])) // Start1 > Start2 + Stop1 <= Stop2
+                .Count()
+                .ToString();
         }
 
         public string Puzzle2(string[] input)
         {
-            var completePairIntersectCount = 0;
-            foreach (var item in input)
-            {
-                var split = item.Split(",");
-
-                var resplit1 = split[0].Split("-");
-                var resplit2 = split[1].Split("-");
-
-                var start1 = Int64.Parse(resplit1[0]);
-                var stop1 = Int64.Parse(resplit1[1]);
-                var start2 = Int64.Parse(resplit2[0]);
-                var stop2 = Int64.Parse(resplit2[1]);
-
-                if (start1 < start2)
-                {
-                    if (stop1 >= start2)
-                    {
-                        completePairIntersectCount++;
-                    }
-                }
-                else if (start1 > start2)
-                {
-                    if (stop2 >= start1)
-                    {
-                        completePairIntersectCount++;
-                    }
-                }
-                else
-                {
-                    // Start is equal, one will contain the other
-                    completePairIntersectCount++;
-                }
-            }
-
-
-            return completePairIntersectCount.ToString();
+            // Minial Code Approach
+            return input.Select(r => r.Split(",")
+                    .Select(q => q.Split("-")
+                        .Select(p => int.Parse(p))
+                        .ToArray())
+                    .ToArray())
+                .Where(r => r[0][0] == r[1][0] || // Start1 == Start2
+                       (r[0][0] < r[1][0] && r[0][1] >= r[1][0]) || // Start1 < Start2 + Stop1 >= Start2
+                       (r[0][0] > r[1][0] && r[0][0] <= r[1][1])) // Start1 > Start2 + Start1 <= Stop2
+                .Count()
+                .ToString();
         }
+
+        //================
+        // Original
+        //================
+        //public string Puzzle1(string[] input)
+        //{
+        //    var completePairContainsCount = 0;
+        //    foreach (var item in input)
+        //    {
+        //        var split = item.Split(",");
+
+        //        var resplit1 = split[0].Split("-");
+        //        var resplit2 = split[1].Split("-");
+
+        //        var start1 = Int64.Parse(resplit1[0]);
+        //        var stop1 = Int64.Parse(resplit1[1]);
+        //        var start2 = Int64.Parse(resplit2[0]);
+        //        var stop2 = Int64.Parse(resplit2[1]);
+
+        //        if (start1 < start2)
+        //        {
+        //            if (stop1 >= stop2)
+        //            {
+        //                completePairContainsCount++;
+        //            }
+        //        }
+        //        else if (start1 > start2)
+        //        {
+        //            if (stop2 >= stop1)
+        //            {
+        //                completePairContainsCount++;
+        //            }
+        //        }
+        //        else
+        //        {
+        //            // Start is equal, one will contain the other
+        //            completePairContainsCount++;
+        //        }
+        //    }
+
+
+        //    return completePairContainsCount.ToString();
+        //}
+
+        //public string Puzzle2(string[] input)
+        //{
+        //    var completePairIntersectCount = 0;
+        //    foreach (var item in input)
+        //    {
+        //        var split = item.Split(",");
+
+        //        var resplit1 = split[0].Split("-");
+        //        var resplit2 = split[1].Split("-");
+
+        //        var start1 = Int64.Parse(resplit1[0]);
+        //        var stop1 = Int64.Parse(resplit1[1]);
+        //        var start2 = Int64.Parse(resplit2[0]);
+        //        var stop2 = Int64.Parse(resplit2[1]);
+
+        //        if (start1 < start2)
+        //        {
+        //            if (stop1 >= start2)
+        //            {
+        //                completePairIntersectCount++;
+        //            }
+        //        }
+        //        else if (start1 > start2)
+        //        {
+        //            if (stop2 >= start1)
+        //            {
+        //                completePairIntersectCount++;
+        //            }
+        //        }
+        //        else
+        //        {
+        //            // Start is equal, one will contain the other
+        //            completePairIntersectCount++;
+        //        }
+        //    }
+
+
+        //    return completePairIntersectCount.ToString();
+        //}
     }
 }
