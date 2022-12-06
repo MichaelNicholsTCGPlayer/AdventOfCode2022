5ba60d400d3a5861573f0f9584a4066aaf2bb897
Michael Nichols <michael.nichols@tcgplayer.com>
2022-12-06 06:32:33 -0500

    Day 6 Optimized

--- Puzzles/Solutions/Day06.cs
+++ Puzzles/Solutions/Day06.cs
@@ -14,13 +14,53 @@
             return Puzzle(input[0], 14).ToString();
         }
 
+        // Optimized
         private int Puzzle(string signal, int distinctCount)
         {
-            return signal
-                .Select((r, i) => new { d = signal[i..(i + distinctCount)].Distinct().Count(), ind = i + distinctCount })
-                .First(r => r.d == distinctCount)
-                .ind;
+            LinkedList<char> queue = new LinkedList<char>();
+            for (int i = 0; i < signal.Length; i++)
+            {
+                var c = signal[i];
+
+                if (queue.Count == distinctCount)
+                {
+                    queue.RemoveFirst();
+                }
+
+                // We only need to check for the most recent character, because we validated all the previous characters
+                var node = queue.Find(c);
+                if (node == null)
+                {
+                    // We didnt find a duplicate, so if the queue if full (one short) then we can return the result
+                    if (queue.Count == distinctCount - 1)
+                    {
+                        return i + 1;
+                    }
+                }
+                else
+                {
+                    // We found a duplicate, so remove it and all of the ones before it (reducing the number of characters we have to check)
+                    while (node.Previous != null)
+                    {
+                        queue.Remove(node.Previous);
+                    }
+                    queue.Remove(node);
+                }
+
+                queue.AddLast(c);
+            }
+
+            throw new Exception("Could Not Find Start Of Signal");
         }
+
+        // Minimal Code
+        //private int Puzzle(string signal, int distinctCount)
+        //{
+        //    return signal
+        //        .Select((r, i) => new { d = signal[i..(i + distinctCount)].Distinct().Count(), ind = i + distinctCount })
+        //        .First(r => r.d == distinctCount)
+        //        .ind;
+        //}
 
         // Original
         //private int Puzzle(string signal, int distinctCount)
