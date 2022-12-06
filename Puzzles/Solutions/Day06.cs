e62420f15cab5188c2ba9a4210e224cf21f33d01
Michael Nichols <michael.nichols@tcgplayer.com>
2022-12-06 05:46:20 -0500

    Day 6 Solution

--- Puzzles/Solutions/Day06.cs
+++ Puzzles/Solutions/Day06.cs
@@ -6,12 +6,32 @@
 
         public string Puzzle1(string[] input)
         {
-            throw new NotImplementedException();
+            return Puzzle(input[0], 4).ToString();
         }
 
         public string Puzzle2(string[] input)
         {
-            throw new NotImplementedException();
+            return Puzzle(input[0], 14).ToString();
+        }
+
+        private int Puzzle(string signal, int distinctCount)
+        {
+            LinkedList<char> queue = new LinkedList<char>();
+            for (int i = 0; i < signal.Length; i++)
+            {
+                queue.AddLast(signal[i]);
+
+                if (queue.Count == distinctCount + 1)
+                {
+                    queue.RemoveFirst();
+                    if (queue.Distinct().Count() == distinctCount)
+                    {
+                        return i + 1;
+                    }
+                }
+            }
+
+            throw new Exception("Could Not Find Start Of Signal");
         }
     }
 }
