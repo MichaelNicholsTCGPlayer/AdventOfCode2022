11d1b38ec05b9850461c822646e7a5a36613b9e3
Michael Nichols <michael.nichols@tcgplayer.com>
2022-12-06 06:03:20 -0500

    Day 6  (Minimal Code)

--- Puzzles/Solutions/Day06.cs
+++ Puzzles/Solutions/Day06.cs
@@ -16,22 +16,31 @@
 
         private int Puzzle(string signal, int distinctCount)
         {
-            LinkedList<char> queue = new LinkedList<char>();
-            for (int i = 0; i < signal.Length; i++)
-            {
-                queue.AddLast(signal[i]);
-
-                if (queue.Count == distinctCount + 1)
-                {
-                    queue.RemoveFirst();
-                    if (queue.Distinct().Count() == distinctCount)
-                    {
-                        return i + 1;
-                    }
-                }
-            }
-
-            throw new Exception("Could Not Find Start Of Signal");
+            return signal
+                .Select((r, i) => new { d = signal[i..(i + distinctCount)].Distinct().Count(), ind = i + distinctCount })
+                .First(r => r.d == distinctCount)
+                .ind;
         }
+
+        // Original
+        //private int Puzzle(string signal, int distinctCount)
+        //{
+        //    LinkedList<char> queue = new LinkedList<char>();
+        //    for (int i = 0; i < signal.Length; i++)
+        //    {
+        //        queue.AddLast(signal[i]);
+
+        //        if (queue.Count == distinctCount + 1)
+        //        {
+        //            queue.RemoveFirst();
+        //            if (queue.Distinct().Count() == distinctCount)
+        //            {
+        //                return i + 1;
+        //            }
+        //        }
+        //    }
+
+        //    throw new Exception("Could Not Find Start Of Signal");
+        //}
     }
 }
